116f86b35a132cc56fc562e7d39a43f1cc02acd9
Siddharth Jha <jha1999siddharth@gmail.com>
2026-06-08 21:38:14 +0530

    Greatest of the three numbers

---
+++ Basics/007_greatest_of_the_three_numbers.py
@@ -0,0 +1,17 @@
+"""
+Problem:
+Find the greatest of the three numbers.
+"""
+
+# Taking input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+# Finding the greatest number
+if (num1 >= num2) and (num1 >= num3):
+    greatest = num1
+elif (num2 >= num1) and (num2 >= num3):
+    greatest = num2
+else:    greatest = num3
+# Displaying the greatest number
+print("The greatest number is:", greatest)
